0d57f75c7ac2340ca8ed8ee936187cf9d594e59d
Chia June <Faire_June@163.com>
2024-01-23 19:35:29 +0800

    代码暂存

--- src/pages/mobile/index.tsx
+++ src/pages/mobile/index.tsx
@@ -11,20 +11,21 @@ const inter = Inter({
 export default function Mobile() {
   return (
     <Fragment>
-      <Script src='/utils.js'></Script>
-      <AppBar position="sticky" color="transparent" sx={{ backdropFilter: 'blur(20px)' }}>
+      <Script src='/utils.js' strategy="beforeInteractive"></Script>
+      <AppBar position="fixed" color="transparent" sx={{ backdropFilter: 'blur(20px)' }}>
         <Container maxWidth="sm" className={inter.className} disableGutters>
           <Toolbar>
-            <Image src="/logo-mobile.png" width={200} height={32} alt="logo" />
-            <Button sx={{ color: '#fff', ml: 'auto' }}>SIGN UP</Button>
-            <IconButton size="small" aria-label="menu" color="primary">
+            <Image src="/mobile/logo-mobile.png" width={100} height={32} alt="logo" />
+            <Button sx={{ color: '#fff', ml: 'auto', fontFamily: 'Arial Rounded MT Bold' }}>SIGN UP</Button>
+            <IconButton size="small" aria-label="menu" sx={{ color: '#fff' }}>
               <Menu />
             </IconButton>
           </Toolbar>
         </Container>
       </AppBar>
-      <Container maxWidth="sm" disableGutters>
-        <img src="/logo3.png" style={{ width: '17.6rem', height: '2.6rem', margin: '0 auto', display: 'block' }} />
+      <Container maxWidth="sm" disableGutters sx={{ position: 'relative', height: 834, overflow: 'hidden' }}>
+        <Image src="/mobile/banner.png" objectFit="contain" width={600} height={834} alt="banner" style={{ position: 'absolute', left: 0, top: 0, zIndex: 0 }} />
+        <img src="/mobile/logo3.png" style={{ width: '17.6rem', height: '2.6rem', margin: '8rem auto 0', display: 'block', position: 'relative', zIndex: 2 }} />
       </Container>
     </Fragment>
   );
