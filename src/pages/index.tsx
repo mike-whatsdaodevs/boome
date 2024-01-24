87b85f1c32499cb7194c807d2b5a837bc3b148cf
Maira Shanahan <lijun@ludashi.com>
2024-01-24 16:25:57 +0800

    提交
    提交

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import { Inter } from 'next/font/google'
 import Home from './home'
-
+import Mobile from './mobile';
+import { useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
-export default function app() {
+export default function App() {
+  const [type, setType] = useState('pc')
+  useEffect(() => {
+    if (
+      navigator.userAgent.match(/Mobi/i) ||
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/iPhone/i)
+    ) {
+      // 当前设备是移动设备
+      setType('phone')
+    } else {
+      setType('pc')
+    }
+  }, [])
   return (
     <>
-      <Home />
+      {type === 'pc' ? <Home /> : <Mobile />}
     </>
   )
 }
